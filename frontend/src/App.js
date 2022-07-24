6f88fd7c5ed43d62c9dc315e70038cdcdc1ca46a
katarinaNinkovic96 <kaca.ninkovic.69@gmail.com>
2022-07-24 22:00:15 +0200

    Replaced href by Link component & fixed Link loading issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap';
 import Header from "./commponents/Header";
 import Footer from "./commponents/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <Router>
+    <>
       <Header />
       <main className='py-3'>
         <Container>
           <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </Router>
+    </>
   );
 }
 
