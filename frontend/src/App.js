f243f0cbbfbc9b551c74388e4d801fdb2322b1f2
katarinaNinkovic96 <kaca.ninkovic.69@gmail.com>
2022-07-24 20:50:01 +0200

    - Added some products on front-end - npm package(s) installation - pages setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap';
+import Header from "./commponents/Header";
+import Footer from "./commponents/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome To ProShop</h1>
-    </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
