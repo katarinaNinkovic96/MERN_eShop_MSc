d025d76eb43784aeeb9395cb9f9cba3e9aa4d545
katarinaNinkovic96 <kaca.ninkovic.69@gmail.com>
2022-08-31 21:55:47 +0200

    User authentication endpoint

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema({
     name: {
@@ -23,6 +24,20 @@ const userSchema = mongoose.Schema({
     timestamps: true
 })
 
+
+//because pass in database is encrypted we need to use decrypt now
+//enteredPassword is plain text password
+//we use decrypt to compare the entered pass to the one in the database thats hashed
+//compare method - compare the plain text to the enccrypted password
+//with this. we can access with all fields in users (pass, name...)
+userSchema.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+
+
+
 const User = mongoose.model('User', userSchema)
 
 export default User
+
