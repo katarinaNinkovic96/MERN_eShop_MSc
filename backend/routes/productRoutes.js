18ea8a9627331096704c46aaae39d3a5868f8b74
katarinaNinkovic96 <kaca.ninkovic.69@gmail.com>
2022-08-31 19:40:42 +0200

    Clean up by using controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,34 +1,18 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
 const router = express.Router()
-import Product from '../models/productModel.js'
+import { getProductById, getProducts } from '../controllers/productController.js'
+
+//all the functionality will go in the controller
+//Controller with all the functions and Routes just have the root and point to the specific controller functions
+
+//router.get('/', getProducts) - it can be like this, but we will work:
+//route.route and for / will handle a 'get request' and call getProducts
+router.route('/').get(getProducts);
 
 
-// @desc    Fetch all products
-// @route   GET /api/products
-// @access  Public
-router.get('/', asyncHandler(async (req, res) => {
 
-    const products = await Product.find({})
-    
-    res.json(products)
-}))
-
-
-// @desc    Fetch single product
-// @route   GET /api/products/:id
-// @access  Public
-router.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-
-    if(product){
-        res.json(product)
-    }else{
-        res.status(404)
-        throw new Error ('Product not found')
-    }
-    
-}))
-
+//same for ID
+//route.get('/:id', getProductById) - it can be like this, but we will work:
+router.route('/:id').get(getProductById)
 
 export default router
