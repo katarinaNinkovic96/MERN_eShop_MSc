50ad1fc5ff05918a89cfd2312be1c720b86cf6ea
katarinaNinkovic96 <kaca.ninkovic.69@gmail.com>
2022-09-01 00:25:41 +0200

    Custom Authentication Middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,40 @@
+//authMiddleware use for validate the tokens
+import jwt from 'jsonwebtoken'
+
+//use asyncHandler to handle any exceptions
+import asyncHandler from 'express-async-handler'
+
+import User from '../models/userModel.js'
+
+//this middleware function - req, res, next
+const protect = asyncHandler(async (req, res, next) => {
+
+    // initialize a variable called token
+    let token
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+
+            //want only token not all - with Bearer [0]
+            token = req.headers.authorization.split(' ')[1]
+
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+            //only we dont return is password
+            req.user = await User.findById(decoded.id).select('-password')
+
+            next()
+        } catch (error) {
+            console.error(error)
+            res.status(401)
+            throw new Error('Not authorized, token failed')
+        }
+    }
+
+    if(!token) {
+        res.status(401)
+        throw new Error('Not authoruzed, no token')
+    }
+})
+
+export { protect }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express'
 const router = express.Router()
-import { authUser } from '../controllers/userController.js'
+import { authUser, getUserProfile } from '../controllers/userController.js'
+import { protect } from '../middleware/authMiddleware.js'
 
 //all the functionality will go in the controller
 //Controller with all the functions and Routes just have the root and point to the specific controller functions
@@ -10,6 +11,9 @@ import { authUser } from '../controllers/userController.js'
 //rouut post 'login' and then we call authUser
 router.post('/login', authUser)
 
+//we use router.route because we will be making more - making a GET req and a put req to update the user profile
+//getUserProfile I want to protect and this method that runs, so to implement middleware (put it as a first argument)
+router.route('/profile').get(protect, getUserProfile)
 
 export default router
 
