f851b7a1bf451496dcf85a066f40cfad182c7ac4
katarinaNinkovic96 <kaca.ninkovic.69@gmail.com>
2022-09-02 20:17:12 +0200

    User Registration & password encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -41,6 +41,54 @@ const authUser = asyncHandler(async (req, res) => {
 })
 
 
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+
+    //body data in postman - the way that we access that in here (userController) is with request body- 
+        //that will give us the data that sent the body
+    //req.body will be able to access like req body email,name, body pass...
+    //for this we need body parser in server.js - app.use(express.json())
+ 
+    //structure that data from req body, I want pull out the name, email and pass (yhose three pieces of data from the body)
+    const { name, email, password } = req.body
+ 
+    //we want to find one document it by email
+    //const user = await User.find({ email : email}) because is match we write only one email
+    //we want to find email that matches with email in req body (line up)
+    const userExists = await User.findOne({ email})
+ 
+    //error 400 is bad request
+    if(userExists) {
+        res.status(400)
+        throw new Error('Users already exists')
+    }
+
+    //if I write only password - this password is unencrypted, its just plain text why we use Mangus middleware to encrypt it.
+    //create like save
+    const user  = await User.create({
+        name,
+        email,
+        password
+    })
+
+    //if user creite, if everithing goes ok
+    //status 201 which means somethiing was created
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }   
+ })
+
 
 // @desc    Get user profile
 // @route   GET /api/users/profile
@@ -64,4 +112,5 @@ const getUserProfile = asyncHandler(async (req, res) => {
  })
 
 
-export { authUser, getUserProfile }
+export { authUser, registerUser, getUserProfile }
+
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -36,6 +36,31 @@ userSchema.methods.matchPassword = async function(enteredPassword){
 
 
 
+//I want encrypt yhe password
+//way that we can add middleware is on the user schema
+//befaure we save, we are going to run an async function and this is going to take in just next
+//This should automatically run pre save, we dont have to bring it in to our user controller 
+userSchema.pre('save', async function (next) {
+
+    //when we have update profile, chande name, email...but not the password we dont want this to run because it is
+        //going to create a new hash and we are going to be able to log in.
+    //if password dont modified we just call next and move on
+    if ( !this.isModified('password')) {
+        next()
+    }
+    //if has been modified, then this will run and it will hach the password.
+
+    //we want to encrypt the passwords, so create salt beacause salt to actually hash the password asynchronously
+    //generate salt and it takes in a number of rounds
+    //beacuse this returns a promise I use await
+    const salt = await bcrypt.genSalt(10)
+
+    //initially this.password (in () after hash) is the plain text password, but now we are resetting 
+        //it to be the hashd password
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+
 
 const User = mongoose.model('User', userSchema)
 
